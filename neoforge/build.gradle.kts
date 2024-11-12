a4e12cf96ab98296af8aa41d53dfd4eac7758aee
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-11-12 18:04:07 -0500

    Initial commit

---
+++ neoforge/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.gradleup.shadow")
+}
+
+architectury {
+    platformSetupLoomIde()
+    neoForge()
+}
+
+val minecraftVersion = project.properties["minecraft_version"] as String
+
+configurations {
+    create("common")
+    "common" {
+        isCanBeResolved = true
+        isCanBeConsumed = false
+    }
+    create("shadowBundle")
+    compileClasspath.get().extendsFrom(configurations["common"])
+    runtimeClasspath.get().extendsFrom(configurations["common"])
+    getByName("developmentNeoForge").extendsFrom(configurations["common"])
+    "shadowBundle" {
+        isCanBeResolved = true
+        isCanBeConsumed = false
+    }
+}
+
+loom {
+    accessWidenerPath.set(project(":common").loom.accessWidenerPath)
+
+    // NeoForge Datagen Gradle config.  Remove if not using NeoForge datagen
+    runs.create("datagen") {
+        data()
+        programArgs("--all", "--mod", "examplemod")
+        programArgs("--output", project(":common").file("src/main/generated/resources").absolutePath)
+        programArgs("--existing", project(":common").file("src/main/resources").absolutePath)
+    }
+}
+
+dependencies {
+    neoForge("net.neoforged:neoforge:${project.properties["neoforge_version"]}")
+
+    "common"(project(":common", "namedElements")) { isTransitive = false }
+    "shadowBundle"(project(":common", "transformProductionNeoForge"))
+}
+
+tasks {
+    processResources {
+        inputs.property("version", project.version)
+
+        filesMatching("META-INF/neoforge.mods.toml") {
+            expand(mapOf("version" to project.version))
+        }
+    }
+
+    shadowJar {
+        exclude("architectury.common.json", "com/example/examplemod/neoforge/datagen/**")
+        configurations = listOf(project.configurations.getByName("shadowBundle"))
+        archiveClassifier.set("dev-shadow")
+    }
+
+    remapJar {
+        inputFile.set(shadowJar.get().archiveFile)
+        dependsOn(shadowJar)
+        atAccessWideners.add("examplemod.accesswidener")
+    }
+}
