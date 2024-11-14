f8bc2707a72e770611a61fc1b43b7eaf2dfddbbc
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-11-14 14:51:57 -0500

    Adjust Eye Height

--- Common/src/main/java/net/potionstudios/wayfinder/world/entity/WayfinderEntities.java
+++ Common/src/main/java/net/potionstudios/wayfinder/world/entity/WayfinderEntities.java
@@ -1,7 +1,6 @@
 package net.potionstudios.wayfinder.world.entity;
 
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.potionstudios.wayfinder.PlatformHandler;
@@ -12,11 +11,7 @@ import java.util.function.Supplier;
 
 public class WayfinderEntities {
 
-    public static final Supplier<EntityType<WayfinderEntity>> WAYFINDER = createEntity("wayfinder", WayfinderEntity::new, MobCategory.AMBIENT, 0.6F, 1F);
-
-    private static <E extends Entity> Supplier<EntityType<E>> createEntity(String id, EntityType.EntityFactory<E> factory, MobCategory category, float width, float height) {
-        return PlatformHandler.PLATFORM_HANDLER.register(BuiltInRegistries.ENTITY_TYPE, id, ()-> EntityType.Builder.of(factory, category).sized(width, height).build(id));
-    }
+    public static final Supplier<EntityType<WayfinderEntity>> WAYFINDER = PlatformHandler.PLATFORM_HANDLER.register(BuiltInRegistries.ENTITY_TYPE, "wayfinder", ()-> EntityType.Builder.of(WayfinderEntity::new, MobCategory.AMBIENT).sized(0.6F, 1.09F).eyeHeight(0.36F).build("wayfinder"));
 
     public static void entities() {
         Wayfinder.LOGGER.info("Registering Wayfinder Entity");
