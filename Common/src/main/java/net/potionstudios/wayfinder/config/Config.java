000c1c40b572d2895938edc497912b6ee1827b1c
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-01-01 18:53:57 -0500

    Setup Wayfinder Heart Block Interaction

--- Common/src/main/java/net/potionstudios/wayfinder/config/Config.java
+++ Common/src/main/java/net/potionstudios/wayfinder/config/Config.java
@@ -7,6 +7,6 @@ public class Config {
     public static class Wayfinder {
         public int MAX_SEARCH_DISTANCE_IN_CHUNKS = 100;
         public int COOLDOWN_IN_SECONDS = 60;
+        public int EMERALD_COST_MULTIPLIER = 1;
     }
-
 }
--- Common/src/main/java/net/potionstudios/wayfinder/world/level/block/WayfinderHeartBlock.java
+++ Common/src/main/java/net/potionstudios/wayfinder/world/level/block/WayfinderHeartBlock.java
@@ -1,17 +1,36 @@
 package net.potionstudios.wayfinder.world.level.block;
 
 import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.TagKey;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.potionstudios.wayfinder.Wayfinder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class WayfinderHeartBlock extends HorizontalDirectionalBlock {
+
+    private static final TagKey<Item> EMERALD_TAG = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("c", "gems/emerald"));
 
     public static final MapCodec<WayfinderHeartBlock> CODEC = simpleCodec(WayfinderHeartBlock::new);
     public static final BooleanProperty ACTIVATED = BooleanProperty.create("activated");
@@ -19,6 +38,41 @@ public class WayfinderHeartBlock extends HorizontalDirectionalBlock {
     public WayfinderHeartBlock(Properties properties) {
         super(properties);
         this.registerDefaultState(this.getStateDefinition().any().setValue(ACTIVATED, false).setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected @NotNull ItemInteractionResult useItemOn(@NotNull ItemStack stack, @NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hitResult) {
+        if (!state.getValue(ACTIVATED) && stack.is(EMERALD_TAG)) {
+            int cost = getCost(level.getDifficulty());
+            if (stack.getCount() >= cost) {
+                level.setBlockAndUpdate(pos, state.setValue(ACTIVATED, true));
+                if (level.isClientSide()) {
+                    RandomSource random = level.getRandom();
+                    for (int i = 0; i < random.nextInt(10, 15); i++)
+                        level.addParticle(
+                                ParticleTypes.HAPPY_VILLAGER,
+                                pos.getX() + 0.5 + (random.nextDouble() - 0.5) * 1.5,
+                                pos.getY() + 1.0 + (random.nextDouble() - 0.5) * 1.5,
+                                pos.getZ() + 0.5 + random.nextDouble() * 0.5,
+                                (random.nextDouble() - 0.5) * 0.2, random.nextDouble() * -0.1, (random.nextDouble() - 0.5) * 0.2
+                        );
+                } else {
+                    if (!player.isCreative()) stack.shrink(cost);
+                    level.playSound(null, pos, SoundEvents.UI_TOAST_CHALLENGE_COMPLETE, SoundSource.BLOCKS);
+                }
+                return ItemInteractionResult.SUCCESS;
+            }
+        }
+        return super.useItemOn(stack, state, level, pos, player, hand, hitResult);
+    }
+
+    private static int getCost(@NotNull Difficulty difficulty) {
+        return switch (difficulty) {
+            case EASY -> 1;
+            case NORMAL -> 2;
+            case HARD -> 3;
+            default -> 0;
+        } * Math.abs(Wayfinder.CONFIG.wayfinder.EMERALD_COST_MULTIPLIER);
     }
 
     @Override
