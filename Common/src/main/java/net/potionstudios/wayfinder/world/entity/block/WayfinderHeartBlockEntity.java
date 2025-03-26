8a6c901cb2edebe0ae5c43dcd64f7cbacb2f04a2
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-03-26 10:35:06 -0400

    Run HeartBlock tick once every 20 ticks

--- Common/src/main/java/net/potionstudios/wayfinder/world/entity/block/WayfinderHeartBlockEntity.java
+++ Common/src/main/java/net/potionstudios/wayfinder/world/entity/block/WayfinderHeartBlockEntity.java
@@ -1,6 +1,7 @@
 package net.potionstudios.wayfinder.world.entity.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -14,10 +15,13 @@ public class WayfinderHeartBlockEntity extends BlockEntity {
 	}
 
 	public static void serverTick(Level level, BlockPos pos, BlockState state, WayfinderHeartBlockEntity blockEntity) {
-		AABB aabb = new AABB(pos).inflate(5);
-		level.getEntitiesOfClass(ServerPlayer.class, aabb).forEach(serverPlayer -> {
-			if (!serverPlayer.isSpectator())
-				WayfinderCriteriaTriggers.WAYFINDER_HEART_BLOCK.get().trigger(serverPlayer);
-		});
+		ServerLevel serverLevel = (ServerLevel) level;
+		if (serverLevel.getServer().getTickCount() % 20 == 0) {
+			AABB aabb = new AABB(pos).inflate(5);
+			serverLevel.getEntitiesOfClass(ServerPlayer.class, aabb).forEach(serverPlayer -> {
+				if (!serverPlayer.isSpectator())
+					WayfinderCriteriaTriggers.WAYFINDER_HEART_BLOCK.get().trigger(serverPlayer);
+			});
+		}
 	}
 }
