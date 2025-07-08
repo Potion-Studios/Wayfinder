b3ac1643a07c27867de9134be8fd9f1ed12afcfc
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-07-07 21:47:57 -0400

    Fix Slight Typo

--- NeoForge/src/main/java/net/potionstudios/wayfinder/neoforge/datagen/NeoForgeDatagen.java
+++ NeoForge/src/main/java/net/potionstudios/wayfinder/neoforge/datagen/NeoForgeDatagen.java
@@ -127,7 +127,7 @@ class NeoForgeDatagen {
             add("gui.wayfinder.search", "Search Biomes");
             add("gui.wayfinder.button.search", "Search");
             add("gui.wayfinder.button.stop", "Stop");
-            add("gui.wayfinder.description", "Congrats on summoning a Wayfinder! This mod aims in providing the player was a unique lore friendly way to explore! Simply select the biome you want to find and follow the Wayfinder! Be careful though, projectile mobs will try to attack him so you will have to defend him! If your Wayfinder dies, you'll need to go find another shrine and summon him with more emeralds!");
+            add("gui.wayfinder.description", "Congrats on summoning a Wayfinder! This mod aims in providing the player with a unique lore friendly way to explore! Simply select the biome you want to find and follow the Wayfinder! Be careful though, projectile mobs will try to attack him so you will have to defend him! If your Wayfinder dies, you'll need to go find another shrine and summon him with more emeralds!");
 
             add("wayfinder.book.story.page1", "Entry 2307,\n" + "I've never seen this before.. I stumbled upon an ancient shrine hidden in the back of this small village. It was covered in vines and moss like no one had touched it for a long time. I was able to make out a faded engraving on the statue..");
             add("wayfinder.book.story.page2", "'When a soul is lost, look to a wayfinder to guide it home'" + "\n" + "I asked the locals about it and one of the librarians directed me to a small corner of the library that looked untouched for decades. It took ages to clean off the spider webs and dust but then");
